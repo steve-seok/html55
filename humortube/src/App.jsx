7dd310244af531c33a5ec49f06fb7562e0130dda
steve-seok <rnfdka@gmail.com>
2025-06-26 11:15:33 +0900

    유머튜브 웹서버에 올림

---
+++ humortube/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className="center_div">
+        <h1>웃긴 유튜브 영상 모음</h1>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/btYhFMDdw_Q?si=-d71ndVapuo7Thy-&amp;start=47" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/lwT20wOI2Ew?si=za70s2jMuVyTdN1s&amp;start=354" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/SXkaSTQkOtA?si=iZ2EiDYRA6wNKzgB&amp;start=379" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/PCUaIkPbRgc?si=xhA2t9q9SMZMd8Wd&amp;start=206" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ test.js
@@ -0,0 +1,6 @@
+let a=10
+const b =20
+const c= a+b
+
+a=50
+console.log(c)
